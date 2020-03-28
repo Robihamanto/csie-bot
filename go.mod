3460e4ff795a863cfaa571faaeab1231e55aeb42
robihamanto <rrobie44@gmail.com>
2020-03-28 16:18:05 +0800

    add time from apple

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/Robihamanto/csie-bot
 
 go 1.14
+
+require (
+	github.com/beevik/ntp v0.3.0 // indirect
+	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e // indirect
+)
--- main.go
+++ main.go
@@ -1,8 +1,25 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"time"
+
+	"github.com/beevik/ntp"
+)
 
 func main() {
-	log.Println("hello")
-	log.Println("Ashiayp")
+	ntpTime, err := ntp.Time("time.apple.com")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	ntpTimeFormatted := ntpTime.Format(time.UnixDate)
+
+	fmt.Printf("Network time: %v\n", ntpTime)
+	fmt.Printf("Unix Date Network time: %v\n", ntpTimeFormatted)
+	fmt.Println("+++++++++++++++++++++++++++++++")
+	timeFormatted := time.Now().Local().Format(time.UnixDate)
+	fmt.Printf("System time: %v\n", time.Now())
+	fmt.Printf("Unix Date System time: %v\n", timeFormatted)
+
 }
